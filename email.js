5bc49f1d4d169265b89eddf985937885eecb4d0a
Jay <jay@ronk.au>
2023-06-27 21:09:25 +0800

    developing node.js server

---
+++ email.js
@@ -0,0 +1,14 @@
+const nodeMailer = require('nodemailer');
+
+const html = `
+    <h1>Hello World</h1>
+    <p>Nodemailer</p>
+`;
+
+async function main() {
+    nodeMailer.createTransport({
+        host: ''
+    })
+}
+
+main();
